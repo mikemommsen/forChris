18a38bae22807e7f8589c01cebe99055d6ca5cc0
mikemommsen <mikemommsen@gmail.com>
2014-05-23 00:05:42 -0500

    Update processer.py

--- processer.py
+++ processer.py
@@ -18,8 +18,11 @@ def arcFunctions(intoa, indem):
     spref = desc.spatialReference
     # and grab the extent object
     extent = desc.extent
+    # turn the extent into an object to feed back to arc
     strextent = ' '.join(map(str, [extent.XMin, extent.YMin, extent.XMax, extent.YMax]))
+    # project the raster
     arcpy.ProjectRaster_management(indem, prjdem, spref, "NEAREST", "30")
+    # clip the raster
     arcpy.Clip_management(prjdem, strextent, utmprj)
     # delete the projected dem
     arcpy.Delete_management(prjdem)
@@ -56,15 +59,25 @@ def makenames(indem):
     return elevproj, utmelev
 
 def findFilePairs(folder):
-    """takes a directory"""
+    """takes a directory creates a list of all file pairs where the directory has one file that ends in toa and one file that ends in elev"""
+    # create a blank list
     processqueue = []
+    # if the folder variable is actually a folder
     if os.path.isdir(folder):
+        # list out all of the things in the folder
         pathlist = os.listdir(folder)
+        # look to see if anything in the folder ends in _toa
         toa = findfile(pathlist, folder, '_toa')
+        # and look to see if anythin ends in _elev
         dem = findfile(pathlist, folder, '_elev')
+        # if the folde has one of each
         if toa and dem:
+            # then we add both those file names to the list
             processqueue.append((toa, dem))
+        # look through every folder that is in that folder
         for subfolder in (x for x in pathlist if os.path.isdir(os.path.join(folder, x))):
+            # and call the function we are running right now on the subfolders
+            # taking those results and appending them to the process queuelist
             processqueue += findFilePairs(os.path.join(folder, subfolder))
     return processqueue
     
@@ -75,10 +88,17 @@ def runArc(indir):
         arcFunctions(toa, dem)
 
 def runNoArc(indir):
+    """basic function that tests the functions in this script that are in charge of filenaming. 
+    allows the script to be run without arc to test filenaming"""
+    # loop through the list of toa and dem files that were found by the findfilePairs function called on the indir
     for toa, dem in findFilePairs(indir):
+        # make the output names using the makenames function
         prjdem, utmprj = makenames(dem)
+        # make a list of the toa, dem, and output names and format it for nicer printing
         mylist = ', '.join((os.path.split(x)[1] for x in (toa, dem, prjdem, utmprj)))
+        # grab the folde rname
         folder = os.path.split(toa)[0]
+        # print to the user one nice line for each successful dir
         print 'folder {}, filenames {}'.format(folder, mylist)
 
 def main():
